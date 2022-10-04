11e1089b14f0c3e076fc6186365b95b25fcd6be5
Javier Puebla <jpuebla1993@gmail.com>
2022-10-04 12:07:10 +0200

    create button and input button components

---
+++ components/buttons.tsx
@@ -0,0 +1,81 @@
+import styled from 'styled-components';
+
+
+const ButtonsContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 48px 0;
+`;
+
+
+interface ButtonProps {
+    children: React.ReactNode;
+    type: string;
+    accept: string;
+    onChange?: () => void;
+}
+
+
+const Button = styled.button<ButtonProps>`
+    background-color: ${({disabled}) => (disabled ? '#6b6b6b' : '#00a8b5')};
+    border-radius: 24px;
+    height: 48px;
+    line-height: 24px;
+    font-size: 16px;
+    font-weight: bold;
+    letter-spacing: normal;
+    padding: 12px 24px;
+    border: none;
+    text-align: center;
+    min-width: 80px;
+    cursor: ${({disabled}) => (disabled ? 'initial' : 'pointer')};
+    margin-left: 24px;
+    
+`;
+
+
+interface InputButtonProps {
+    children: React.ReactNode;
+    type: string;
+    accept: string;
+    onChange?: (event: any) => void;
+}
+
+
+const LabelButton = styled.label`
+    background-color: #00a8b5;
+    border-radius: 24px;
+    height: 48px;
+    line-height: 24px;
+    font-size: 16px;
+    font-weight: bold;
+    letter-spacing: normal;
+    padding: 12px 24px;
+    margin-right: 24px;
+    border: none;
+    text-align: center;
+    min-width: 80px;
+    cursor: pointer;
+
+    > input{display: none !important;}
+`;
+
+
+const InputButton = (props: InputButtonProps): JSX.Element => {
+    const {
+        type,
+        accept,
+        children,
+        onChange,
+    } = props;
+
+    return (
+        <LabelButton>
+            <input type={type} onChange={onChange} accept={accept} />
+            {children}
+        </LabelButton>
+    );
+};
+
+
+export { ButtonsContainer, Button, InputButton };
