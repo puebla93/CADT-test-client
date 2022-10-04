be06e596991e5b472d07e62c0190e4c41641ae5c
Javier Puebla <jpuebla1993@gmail.com>
2022-10-04 12:11:28 +0200

    using button and input button in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import ImagesContainer from '../components/image_container'
+import { ButtonsContainer, Button, InputButton } from '../components/buttons'
 import styles from '../styles/Home.module.css'
 
 const SERVER_BASE_URL = 'http://localhost:5000';
@@ -10,20 +11,21 @@ const API_V1_PATH = '/api/v1/';
 const API_V1_URL = `${SERVER_BASE_URL}${API_V1_PATH}`;
 
 const Home: NextPage = () => {
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState<string|Blob>('');
   const [imageURL, setImageURL] = useState('/placeholder.png');
   const [filteredImage, setFilteredImage] = useState('/placeholder.png');
   const [filterFunc, setFilterFunc] = useState('emboss');
 
-  const uploadToClient = (event) => {
+  const uploadToClient = (event: any) => {
     if (event.target.files && event.target.files[0]) {
       const img = event.target.files[0];
       setImage(img);
+      console.log(event);
       setImageURL(URL.createObjectURL(img));
     }
   };
 
-  const processImage = async (event) => {
+  const processImage = async (event: any) => {
     const body = new FormData();
     body.append('image', image);
     body.append('filterFunc', filterFunc);
@@ -50,20 +52,21 @@ const Home: NextPage = () => {
         <img src={imageURL} alt='' />
         <img src={filteredImage} alt='' />
       </ImagesContainer>
-      <h4>Select Image</h4>
-      {/* TODO style this tags */}
-      <input type='file' name='myImage' onChange={uploadToClient} accept='image/png, image/jpeg' />
-      <select
-        value={filterFunc}
-        onChange={(e) => {setFilterFunc(e.target.value);}}
-      >
-        <option value="emboss">Emboss</option>
-        <option value="gaussianBlur">Gaussian Blur</option>
-        <option value="cannyEdge">Canny Edge</option>
-        <option value="grayScale">Gray Scale</option>
-        <option value="sepia">Sepia</option>
-      </select>
-      <button className='btn btn-primary' type='submit' onClick={processImage}>Process image</button>
+      <ButtonsContainer>
+        <InputButton type='file' onChange={uploadToClient} accept='image/png, image/jpeg'>Upload image</InputButton>
+        {/* TODO style this tags */}
+        <select
+          value={filterFunc}
+          onChange={(e) => {setFilterFunc(e.target.value);}}
+        >
+          <option value='emboss'>Emboss</option>
+          <option value='gaussianBlur'>Gaussian Blur</option>
+          <option value='cannyEdge'>Canny Edge</option>
+          <option value='grayScale'>Gray Scale</option>
+          <option value='sepia'>Sepia</option>
+        </select>
+        <Button type='submit' onClick={processImage} disabled={!image} >Process image</Button>
+      </ButtonsContainer>
     </div>
   )
 }
